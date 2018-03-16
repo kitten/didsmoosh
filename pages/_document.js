dd7244e2555c3fe0528297f3cd20bb955e57ce26
Phil Plückthun <phil@kitten.sh>
2018-03-16 11:46:48 +0000

    Add opengraph and font from Google

--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,27 @@ import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet, injectGlobal } from 'styled-components'
 
 injectGlobal`
+  @font-face {
+    font-family: 'Source Sans Pro';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'), url(https://fonts.gstatic.com/s/sourcesanspro/v11/6xK3dSBYKcSV-LCoeQqfX1RYOo3qOK7lujVj9w.woff2) format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Source Sans Pro';
+    font-style: normal;
+    font-weight: 600;
+    src: local('Source Sans Pro SemiBold'), local('SourceSansPro-SemiBold'), url(https://fonts.gstatic.com/s/sourcesanspro/v11/6xKydSBYKcSV-LCoeQqfX1RYOo3i54rwlxdu3cOWxw.woff2) format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Source Sans Pro';
+    font-style: normal;
+    font-weight: 800;
+    src: local('Source Sans Pro Black'), local('SourceSansPro-Black'), url(https://fonts.gstatic.com/s/sourcesanspro/v11/6xKydSBYKcSV-LCoeQqfX1RYOo3iu4nwlxdu3cOWxw.woff2) format('woff2');
+  }
+
   body, html {
     margin: 0;
     font-family: 'Source Sans Pro', 'source-sans-pro', sans-serif;
@@ -38,9 +59,21 @@ export default class MyDocument extends Document {
         <Head>
           <meta charset="UTF-8" />
           <title>componentDidSmoosh()</title>
-          <meta name="description" content="componentDidSmoosh is a JavaScript comedy night like never seen before, bringing London’s greatest experts of developer humour on stage" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="stylesheet" href="https://use.typekit.net/sis5bcz.css" />
+
+          <meta name="description" content="componentDidSmoosh is a JavaScript comedy night like never seen before, bringing London’s greatest experts of developer humour on stage to make you laugh and cry." />
+          <meta name="twitter:card" content="summary" />
+          <meta property="og:title" content= "componentDidSmoosh — A JavaScript comedy night"/>
+          <meta property="og:description" content="componentDidSmoosh is a JavaScript comedy night like never seen before, bringing London’s greatest experts of developer humour on stage to make you laugh and cry." />
+          <meta property="og:type" content="event"/>
+          <meta property="og:image" content="https://smoosh.fun/static/banner.jpg" />
+          <meta property="og:url" content="https://smoosh.fun" />
+          <meta property="og:fn" content="Ticketmaster UK" />
+          <meta property="og:street-address" content="4 Pentonville Rd"/>
+          <meta property="og:locality" content="London"/>
+          <meta property="og:postal-code" content="N1 9HF" />
+          <meta property="og:country-name" content="UK"/>
+          <meta property="og:start-time" content="2010-04-06T18:00"/>
 
           {this.props.styleTags}
         </Head>
