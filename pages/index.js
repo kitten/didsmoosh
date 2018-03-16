bc22a1d092328d7a8ca446cab1bfb366bcb31dba
Jani Eväkallio <jani.evakallio@gmail.com>
2018-03-16 11:01:22 +0000

    Update content, add links to signup page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import styled from 'styled-components'
-import { rem } from 'polished'
+import styled from "styled-components";
+import { rem } from "polished";
 
-import Hero from '../components/Hero'
-import Button from '../components/Button'
-import Decoration from '../components/decoration'
+import Hero from "../components/Hero";
+import Button from "../components/Button";
+import Decoration from "../components/decoration";
 
 const Wrapper = styled.div`
   min-height: 100vh;
-`
+`;
 
 const Content = styled.div`
   display: flex;
@@ -18,11 +18,11 @@ const Content = styled.div`
   padding: ${rem(60)} ${rem(20)};
   max-width: ${rem(620)};
   margin: 0 auto;
-`
+`;
 
 const B = styled.b`
   font-weight: 800;
-`
+`;
 
 const Description = styled.p`
   font-size: ${rem(28)};
@@ -30,7 +30,7 @@ const Description = styled.p`
   font-weight: 600;
   padding: ${rem(40)} 0;
   text-align: center;
-`
+`;
 
 const Section = styled.div`
   position: relative;
@@ -41,7 +41,7 @@ const Section = styled.div`
 
   background-repeat: no-repeat;
   background-position: 50% 0%;
-  background-image: url('/static/decoration.png');
+  background-image: url("/static/decoration.png");
   background-size: ${rem(100)} ${rem(400)};
 
   color: #0e1e25;
@@ -49,7 +49,7 @@ const Section = styled.div`
   max-width: ${rem(455)};
   margin: ${rem(45)} 0 ${rem(30)};
   padding: ${rem(16)} 0;
-`
+`;
 
 const SectionHeading = styled.h2`
   font-size: ${rem(72)};
@@ -59,80 +59,107 @@ const SectionHeading = styled.h2`
   margin: ${rem(22)} 0;
   text-align: center;
   text-transform: uppercase;
-`
+`;
 
 const SectionContent = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  align-items: ${p => p.center ? 'center' : 'flex-end'};
-  text-align: ${p => p.center ? 'center' : 'right'};
+  align-items: ${p => (p.center ? "center" : "flex-end")};
+  text-align: ${p => (p.center ? "center" : "right")};
   margin: ${rem(30)} 0;
-`
+`;
 
 const Body = styled.h3`
   font-weight: 600;
   font-size: ${rem(28)};
   line-height: ${rem(34)};
   letter-spacing: ${rem(0.88)};
-`
+`;
 
 const BoldBody = styled(Body)`
   text-transform: uppercase;
   font-weight: 800;
-`
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  color: black;
+  :hover {
+    text-decoration: underline;
+  }
+`;
 
 export default () => (
   <Wrapper>
     <Hero />
     <Content>
       <Description>
-        <B>componentDidSmoosh</B> is a JavaScript comedy night, as never seen before, bringing London’s
-        greatest experts of developer humour on stage to make you laugh and cry.
+        <B>componentDidSmoosh</B> is a JavaScript comedy night like never seen
+        before, bringing London’s greatest experts of developer humour on stage
+        to make you laugh and cry.
       </Description>
 
-      <Button href="#">
-        Join the fun!
+      <Button href="https://www.meetup.com/formidable-london/events/248799751/">
+        Join the fun
       </Button>
 
       <Section>
-        <SectionHeading>
-          Location
-        </SectionHeading>
+        <SectionHeading>Location</SectionHeading>
         <SectionContent center>
-          <BoldBody>
-            Ticketmaster UK
-          </BoldBody>
+          <BoldBody>Ticketmaster UK</BoldBody>
           <Body>
             4 Pentonville Rd,<br />
             London N1 9HF<br />
-            6:00 PM – 9:00 PM
+            6:00 PM – 10:00 PM
           </Body>
         </SectionContent>
       </Section>
 
       <Section>
-        <SectionHeading>
-          Program
-        </SectionHeading>
-        <SectionContent>
-          <BoldBody>The Standup Intro.</BoldBody>
-          <Body>—Jani Evakallio</Body>
-        </SectionContent>
+        <SectionHeading>Program</SectionHeading>
         <SectionContent>
           <BoldBody>Battle Decks.</BoldBody>
-          <Body>—Taz Singh</Body>
+          <Body>
+            —<Link href="https://twitter.com/tazsingh">Taz Singh</Link>
+          </Body>
         </SectionContent>
         <SectionContent>
           <BoldBody>Fuck this thing in particular.</BoldBody>
-          <Body>—Sara Vieira</Body>
+          <Body>
+            —<Link href="https://twitter.com/NikkitaFTW">Sara Vieira</Link>
+          </Body>
         </SectionContent>
         <SectionContent>
-          <BoldBody>Wheeler Remote Radio.</BoldBody>
-          <Body>—Ken Wheeler</Body>
+          <BoldBody>Feeling the blues.</BoldBody>
+          <Body>
+            —<Link href="https://twitter.com/threepointone">Sunil Pai</Link>
+          </Body>
+        </SectionContent>
+        <SectionContent>
+          <BoldBody>JavaScript amirite?</BoldBody>
+          <Body>
+            —<Link href="https://twitter.com/jevakallio">Jani Evakallio</Link>
+          </Body>
+        </SectionContent>
+        <SectionContent>
+          <BoldBody>
+            An improvised comedy about the tragedy of programming.
+          </BoldBody>
+          <Body>—TC Thirty-NaN</Body>
+        </SectionContent>
+        <SectionContent>
+          <BoldBody>Beats by Ken.</BoldBody>
+          <Body>
+            —<Link href="https://twitter.com/ken_wheeler">Ken Wheeler</Link>{" "}
+            (remote)
+          </Body>
         </SectionContent>
       </Section>
 
+      <Button href="https://www.meetup.com/formidable-london/events/248799751/">
+        Sign the heck up
+      </Button>
     </Content>
   </Wrapper>
-)
+);
