8290ab8735832ffea80edb7a0c918e76fe571eff
Phil Pluckthun <phil@kitten.sh>
2018-11-08 01:55:16 +0000

    Add devicemotion event to MouseMove for mobile devices

--- src/components/Hero/MouseMove.js
+++ src/components/Hero/MouseMove.js
@@ -2,8 +2,13 @@ import { Component } from 'react';
 
 class MouseMove extends Component {
   state = { x: 0.5, y: 0.5 };
+  ignoreMove = false;
 
   onMouseMove = ({ clientX, clientY }) => {
+    if (this.ignoreMove) {
+      return;
+    }
+
     const { innerWidth, innerHeight } = this.state;
     if (!innerWidth || !innerHeight) {
       return;
@@ -11,6 +16,16 @@ class MouseMove extends Component {
 
     const x = Math.min(clientX, innerWidth) / innerWidth;
     const y = Math.min(clientY, innerHeight) / innerHeight;
+    this.setState({ x, y });
+  };
+
+  onDeviceMotion = ({ rotationRate: { beta, gamma } }) => {
+    // Start ignoring mousemove events
+    window.removeEventListener('mousemove', this.onMouseMove);
+
+    const x = Math.min(Math.max(gamma, -45), 45) / -90;
+    const y = Math.min(Math.max(beta, -45), 45) / -90;
+
     this.setState({ x, y });
   };
 
@@ -22,18 +37,18 @@ class MouseMove extends Component {
   componentDidMount() {
     this.onResize();
     window.addEventListener('mousemove', this.onMouseMove, { passive: true });
+    window.addEventListener('devicemotion', this.onDeviceMotion);
     window.addEventListener('resize', this.onResize, { passive: true });
   }
 
   componentWillUnmount() {
     window.removeEventListener('mousemove', this.onMouseMove);
+    window.removeEventListener('devicemotion', this.onDeviceMotion);
     window.removeEventListener('resize', this.onResize);
   }
 
   render() {
-    const { children } = this.props;
-    const { x, y } = this.state;
-    return children({ x, y });
+    return this.props.children(this.state);
   }
 }
 
