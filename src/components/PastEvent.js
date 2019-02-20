c05a4d4fc527295565e60cc28f1bc1b5516de342
Tasveer Singh <taz@tazsingh.com>
2019-02-20 17:47:56 -0500

    Refactor past events a bit

---
+++ src/components/PastEvent.js
@@ -0,0 +1,80 @@
+import React, { Fragment } from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import { styled } from 'linaria/react';
+
+import { Spacer } from '../components/HeroLayout/common';
+
+const ImageGrid = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+`;
+
+const ImageCell = styled.div`
+  position: relative;
+  margin-bottom: 1.25rem;
+  flex-basis: 100%;
+
+  @media (min-width: 400px and max-width: 768px) {
+    flex-basis: 50%;
+    margin-right: 1.5rem;
+
+    &:nth-child(2) {
+      margin-right: 0;
+    }
+  }
+
+  @media (min-width: 768px) {
+    flex-basis: 33.33%;
+    margin-right: 1.5rem;
+  }
+`;
+
+const Info = styled.h3`
+  font-weight: 600;
+  letter-spacing: 0.1rem;
+  line-height: 3.9rem;
+  font-size: 3.75rem;
+  margin-bottom: 2rem;
+
+  @media (max-width: 768px) {
+    font-size: 2.2rem;
+    line-height: 1.25;
+  }
+`;
+
+export const query = graphql`
+  fragment PastEventImageFragment on FileConnection {
+    edges {
+      node {
+        childImageSharp {
+          fluid(maxWidth: 340) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default ({ title, location, data }) => {
+  return (
+    <Fragment>
+      <Info>
+        {title}
+        <br />
+        {location}
+        <Spacer />
+      </Info>
+      <ImageGrid>
+        {data.edges.map(({ node }, i) => (
+          <ImageCell key={i}>
+            <Img fluid={node.childImageSharp.fluid} />
+          </ImageCell>
+        ))}
+      </ImageGrid>
+    </Fragment>
+  );
+};
